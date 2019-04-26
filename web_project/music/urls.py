514bf470b6425aa586b0a691edcd76c2c8e21ab3
Abhishek Jadhav <43902352+I-AM-0xBIT@users.noreply.github.com>
2019-04-26 23:39:26 +0530

    Admin and Details view added

--- web_project/music/urls.py
+++ web_project/music/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    #music/20/
+    path('<album_id>/',views.detail, name = 'detail'),
 ]
--- web_project/music/views.py
+++ web_project/music/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> This is the music app homepage<h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
