6e4692dfbcbe54a285ef104fad8af148d272d34f
Abhishek Jadhav <43902352+I-AM-0xBIT@users.noreply.github.com>
2019-05-01 15:26:33 +0530

    Created detail http page and modified detail method

--- web_project/music/views.py
+++ web_project/music/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Album
 
 # Create your views here.
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
+    context = {
+        'all_albums' : all_albums,
+    }
+    return render(request, 'music/index.html', context)
+
+    '''html = ''
     for album in all_albums:
         url= '/music/' + str(album.id) + '/'
         html += '<a href="'+ url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    return HttpResponse(html) '''
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album' : album})
